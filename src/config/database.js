a31a013387bb221967f4effe4331a08b3719b40b
Moh Aadil <aadilrehman.development@gmail.com>
2025-03-06 18:09:27 +0530

    modify database.js

--- src/config/database.js
+++ src/config/database.js
@@ -1,9 +1,8 @@
 const mongoose = require("mongoose");
+require("dotenv").config();
 
 const mongoDBconnect = async () => {
-	await mongoose.connect(
-		"mongodb+srv://aadilrehman:tmeIajsN2DiBdOkB@cluster0.i9gxd.mongodb.net/devTinder?retryWrites=true&w=majority&appName=Cluster0"
-	);
+	await mongoose.connect(process.env.MONGO_URI);
 };
 
 module.exports = mongoDBconnect;
