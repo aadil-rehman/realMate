b8367e22e702a2678731069e745ca355b7bf0aef
Moh Aadil <aadilrehman.development@gmail.com>
2025-03-11 14:54:20 +0530

    schema validations

--- src/app.js
+++ src/app.js
@@ -7,12 +7,12 @@ const User = require("./models/user");
 app.use(express.json());
 
 //User signup
-app.post("/signUp", (req, res) => {
+app.post("/signUp", async (req, res) => {
 	const userObj = req.body;
 	const user = new User(userObj);
 
 	try {
-		user.save();
+		await user.save();
 		res.send("User Added Successfully");
 	} catch (err) {
 		res.status(400).send("Error in savibg the user:" + err.message);
@@ -45,6 +45,57 @@ app.get("/feed", async (req, res) => {
 	}
 });
 
+//delet user
+app.delete("/user", async (req, res) => {
+	const userEmail = req.body.emailId;
+	try {
+		const user = await User.findOneAndDelete({ emailId: userEmail });
+		res.send(
+			`User ${user.firstName + " " + user.lastName} deleted successfully.`
+		);
+	} catch (err) {
+		res.status(400).send("Something went wrong");
+	}
+});
+
+//partial update
+app.patch("/user", async (req, res) => {
+	const userId = req.body._id;
+	const data = req.body;
+
+	if (!userId) {
+		return res.status(400).send("User ID is required.");
+	}
+	try {
+		const user = await User.findByIdAndUpdate(userId, data, {
+			new: true,
+			runValidators: true, //to run the validators applied in schemas in each patch update
+		});
+
+		res.send(`User Updated successfully.`);
+	} catch (err) {
+		res.status(400).send("Update Failed:" + err.message);
+	}
+});
+
+//Fully update
+app.put("/user", async (req, res) => {
+	const userEmail = req.body.emailId;
+	const data = req.body;
+
+	if (!userEmail) {
+		return res.status(400).send("User email is required.");
+	}
+	try {
+		const user = await User.findOneAndReplace({ emailId: userEmail }, data, {
+			new: true,
+		});
+
+		res.send(`User Updated successfully.`);
+	} catch (err) {
+		res.status(400).send("Update Failed:" + err.message);
+	}
+});
 mongoDBconnect()
 	.then(() => {
 		console.log("Database connection established");
--- src/models/user.js
+++ src/models/user.js
@@ -1,25 +1,43 @@
 const mongoose = require("mongoose");
 
-const userSchema = new mongoose.Schema({
-	firstName: {
-		type: String,
+const userSchema = new mongoose.Schema(
+	{
+		firstName: {
+			type: String,
+			required: true,
+		},
+		lastName: {
+			type: String,
+		},
+		emailId: {
+			type: String,
+			required: true,
+			lowercase: true,
+			unique: true,
+			trim: true,
+		},
+		password: {
+			type: String,
+			required: true,
+		},
+		age: {
+			type: String,
+		},
+		gender: {
+			type: String,
+			validate(value) {
+				if (!["male", "female", "other"].includes(value)) {
+					throw new Error("Gender data is not valid");
+				}
+			},
+		},
+		about: {
+			type: String,
+			default: "This is default about user",
+		},
 	},
-	lastName: {
-		type: String,
-	},
-	emailId: {
-		type: String,
-	},
-	password: {
-		type: String,
-	},
-	age: {
-		type: String,
-	},
-	gender: {
-		type: String,
-	},
-});
+	{ timestamps: true } //createdAt and updatedAt timing will appear automatically by using this
+);
 
 const userModel = mongoose.model("User", userSchema);
 
